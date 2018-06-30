00a0a74e6cf0d60c2b020d0de0038bcbc0cf0fde
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-30 18:23:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli)
+  the_string = "The line is currently: "
+  if deli.size == 0 
+    puts "The line is currently empty."
+    return
+  else 
+    for i in (0...deli.size)
+      if i == deli.size - 1 
+        the_string += "#{i+1}. #{deli[i]}"
+      else 
+        the_string += "#{i+1}. #{deli[i]} "
+      end
+    end
+  end
+  puts the_string
+end
+  take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
+
+def deli_counter(line, name)
+  "Welcome, #{name}. You are number #{line.size} in line."
+end
