fbe42c62ee4ec97858fcc54fe39ccbc0679ff46a
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-30 18:28:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,3 +21,7 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
+
+def now_serving
+  puts "Currently serving #{katz_deli.shift}."
+end
